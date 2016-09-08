0861488e889dcb1a861a82299213ceaeb09ec166
Kazuhiro Yamada <yamadakazu45@gmail.com>
2016-09-08 18:58:31 +0900

    Initial Commit

---
+++ PagingViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagingViewController
+//
+//  Created by Kazuhiro Yamada on 2016/09/08.
+//  Copyright © 2016年 Kazuhiro Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
